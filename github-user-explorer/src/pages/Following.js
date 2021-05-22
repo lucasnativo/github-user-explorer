68bfe5d46e72a07f4ba392159977172f16e068f9
Lucas Sampaio <lucasssampaio@gmail.com>
2021-05-22 01:05:48 -0300

    login como seguidor

--- github-user-explorer/src/pages/Following.js
+++ github-user-explorer/src/pages/Following.js
@@ -1,8 +1,9 @@
 import NavBar from "../components/NavBar";
 import { useContext, useState } from "react";
-import { Redirect, Link } from "react-router-dom";
+import { Redirect, Link, useHistory } from "react-router-dom";
 import { DataContext } from "../context/DataContext";
 import fetchUserData from "../utils/fetchUserData";
+import fetchAllUserData from "../utils/fetchAllUserData";
 import Profile from "../components/Profile";
 
 // let a = {
@@ -13,6 +14,7 @@ import Profile from "../components/Profile";
 function Following() {
   const { data, setData } = useContext(DataContext);
   const [selectedUser, setSelectedUser] = useState(null);
+  const history = useHistory();
 
   if (!data) {
     return <Redirect to="/"></Redirect>;
@@ -29,17 +31,27 @@ function Following() {
     </Link>
   );
 
-  // if (selectedUser) {
-  //   Voltar = (
-  //     <a href="/following" onClick={() => setSelectedUser(null)}>
-  //       Voltar
-  //     </a>
-  //   );
-  // }
+  if (selectedUser) {
+    Voltar = <button onClick={() => setSelectedUser(null)}>Voltar</button>;
+  }
+
+  let Entrar = (
+    <button onClick={() => switchUser(selectedUser.login)}>Entrar</button>
+  );
+
+  async function switchUser(login) {
+    const user = await fetchAllUserData(login);
+    setData(user);
+    history.push("/home");
+    console.log(user);
+  }
 
   return (
     <>
-      <div>{Voltar}</div>
+      <div>
+        {Voltar}
+        {selectedUser && Entrar}
+      </div>
       {selectedUser ? (
         <Profile user={selectedUser} />
       ) : (
